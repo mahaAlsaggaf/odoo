d796b28156759e89916294d1231f871d37e1e553
mahaAlsaggafx <mahaAlsaggafx@gmail.com>
2022-08-04 17:03:00 +0300

    Working on Chapter 9

--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -6,17 +6,17 @@ from dateutil.relativedelta import relativedelta
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Test Model"
-    #_postcode = "estate.property"
-    #_date_availability = "estate.property"
-    #_expected_price = "estate.property"
-    #_selling_price = "estate.property"
-    #_bedrooms = "estate.property"
-    #_living_area = "estate.property"
-    #_facades = "estate.property"
-    #_garage = "estate.property"
-    #_garden = "estate.property"
-    #_garden_area = "estate.property"
-    #_garden_orientation = "estate.property"
+    # _postcode = "estate.property"
+    # _date_availability = "estate.property"
+    # _expected_price = "estate.property"
+    # _selling_price = "estate.property"
+    # _bedrooms = "estate.property"
+    # _living_area = "estate.property"
+    # _facades = "estate.property"
+    # _garage = "estate.property"
+    # _garden = "estate.property"
+    # _garden_area = "estate.property"
+    # _garden_orientation = "estate.property"
 
     name = fields.Char(required=True)
     description = fields.Text(string='Description')
@@ -49,7 +49,18 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one(comodel_name="res.users", copy=False, string="Buyer")
     tag_id = fields.Many2many("estate.property.tag", string="Tags")
     offer_id = fields.One2many("estate.property.offer", "property_id", copy=False, string="Offer")
+    total_area = fields.Integer(compute="_compute_total_area", readonly=True)
+    best_price = fields.Float(compute="_compute_best_price", readonly=True)
 
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for estate in self:
+            estate.total_area = estate.living_area + estate.garden_area
 
-
+    # mapped(): applies the provided function to each record in the recordset, returns a recordset if the results are
+    # recordsets. The provided function can be a string to get field values.
+    @api.depends('offer_id.price')
+    def _compute_best_price(self):
+        for estate in self:
+            estate.best_price = max(estate.offer_id.mapped('price'))
 
